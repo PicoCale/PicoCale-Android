d28db51959e933b6ce185baca60a542f783c371a
Srikrishnan Suresh <krishna.tanjavur@gmail.com>
2015-07-25 15:26:01 -0400

    Initial Project Setup

---
+++ app/src/main/java/edu/cmu/mobileapp/picocale/view/fragment/HomeFragment.java
@@ -0,0 +1,22 @@
+package edu.cmu.mobileapp.picocale.view.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.cmu.mobileapp.picocale.R;
+
+
+/**
+ * Created by srikrishnan_suresh on 25-07-2015.
+ */
+public class HomeFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+}
