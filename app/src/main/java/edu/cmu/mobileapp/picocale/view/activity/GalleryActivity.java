955ccf0e6347e4fd95a301a42ff0cc671ba8242a
Srikrishnan Suresh <krishna.tanjavur@gmail.com>
2015-07-27 09:08:53 -0400

    Adding the title and subtitle in the gallery activity

--- app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/GalleryActivity.java
+++ app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/GalleryActivity.java
@@ -2,23 +2,28 @@ package edu.cmu.mobileapp.picocale.view.activity;
 
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 
 import edu.cmu.mobileapp.picocale.R;
 
 /**
  * Created by srikrishnan_suresh on 07/26/2015.
  */
-public class GalleryActivity extends FragmentActivity {
+public class GalleryActivity extends ActionBarActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
--- app/src/main/java/edu/cmu/mobileapp/picocale/view/fragment/GalleryFragment.java
+++ app/src/main/java/edu/cmu/mobileapp/picocale/view/fragment/GalleryFragment.java
@@ -1,9 +1,11 @@
 package edu.cmu.mobileapp.picocale.view.fragment;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,6 +24,7 @@ import edu.cmu.mobileapp.picocale.view.activity.MainActivity;
 public class GalleryFragment extends Fragment {
 
     private GridView gridView;
+    private Activity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,11 +32,15 @@ public class GalleryFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_gallery, container, false);
 
+        activity = getActivity();
         gridView = (GridView) view.findViewById(R.id.galleryGrid);
 
-        Intent intent = getActivity().getIntent();
+        Intent intent = activity.getIntent();
         String locationString = intent.getStringExtra("location");
-        new DeviceImageFetcher(getActivity(),gridView).execute(locationString);
+        ((ActionBarActivity)activity).getSupportActionBar().setDisplayShowTitleEnabled(true);
+        ((ActionBarActivity)activity).getSupportActionBar().setTitle(R.string.app_name);
+        ((ActionBarActivity)activity).getSupportActionBar().setSubtitle(locationString);
+        new DeviceImageFetcher(activity,gridView).execute(locationString);
         return view;
     }
 
