8142eba1bab122805d25223dd260f48d811976ae
svsivaraman <sv.sivaraman107@gmail.com>
2015-07-28 00:11:20 -0400

    Adding utils classes for flickr

---
+++ app/src/main/java/edu/cmu/mobileapp/picocale/util/FlickrHelper.java
@@ -0,0 +1,68 @@
+package edu.cmu.mobileapp.picocale.util;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import com.googlecode.flickrjandroid.Flickr;
+import com.googlecode.flickrjandroid.REST;
+import com.googlecode.flickrjandroid.RequestContext;
+import com.googlecode.flickrjandroid.interestingness.InterestingnessInterface;
+import com.googlecode.flickrjandroid.oauth.OAuth;
+import com.googlecode.flickrjandroid.oauth.OAuthToken;
+import com.googlecode.flickrjandroid.photos.PhotosInterface;
+
+public final class FlickrHelper {
+
+    private static FlickrHelper instance = null;
+    private static final String API_KEY = "c0b714fe7b7d577c8ab3a41943527e78"; //$NON-NLS-1$
+    public static final String API_SEC = "ff9391990520466c"; //$NON-NLS-1$
+
+    private FlickrHelper() {
+
+    }
+
+    public static FlickrHelper getInstance() {
+        if (instance == null) {
+            instance = new FlickrHelper();
+        }
+
+        return instance;
+    }
+
+    public Flickr getFlickr() {
+        try {
+            Flickr f = new Flickr(API_KEY, API_SEC, new REST());
+            return f;
+        } catch (ParserConfigurationException e) {
+            return null;
+        }
+    }
+
+    public Flickr getFlickrAuthed(String token, String secret) {
+        Flickr f = getFlickr();
+        RequestContext requestContext = RequestContext.getRequestContext();
+        OAuth auth = new OAuth();
+        auth.setToken(new OAuthToken(token, secret));
+        requestContext.setOAuth(auth);
+        return f;
+    }
+
+    public InterestingnessInterface getInterestingInterface() {
+        Flickr f = getFlickr();
+        if (f != null) {
+            return f.getInterestingnessInterface();
+        } else {
+            return null;
+        }
+    }
+
+    public PhotosInterface getPhotosInterface() {
+        Flickr f = getFlickr();
+        if (f != null) {
+            return f.getPhotosInterface();
+        } else {
+            return null;
+        }
+    }
+
+}
+
---
+++ app/src/main/java/edu/cmu/mobileapp/picocale/util/ImageCache.java
@@ -0,0 +1,68 @@
+package edu.cmu.mobileapp.picocale.util;
+
+import java.lang.ref.SoftReference;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import android.graphics.Bitmap;
+
+public final class ImageCache {
+
+    private static final Map<String, SoftReference<Bitmap>> cache = new LinkedHashMap<String, SoftReference<Bitmap>>() {
+        private static final long serialVersionUID = 1L;
+
+        /* (non-Javadoc)
+         * @see java.util.LinkedHashMap#removeEldestEntry(java.util.Map.Entry)
+         */
+        @Override
+        protected boolean removeEldestEntry(
+                Entry<String, SoftReference<Bitmap>> eldest) {
+            return size() > 50;
+        }
+
+    };
+
+    private static final ReentrantReadWriteLock readWriteLock = new ReentrantReadWriteLock();
+    private static final Lock read  = readWriteLock.readLock();
+    private static final Lock write = readWriteLock.writeLock();
+
+    /**
+     *
+     */
+    private ImageCache() {
+        super();
+    }
+
+    public static Bitmap getFromCache(String id){
+        read.lock();
+        try {
+            if(!cache.containsKey(id))
+                return null;
+            SoftReference<Bitmap> ref=cache.get(id);
+            return ref.get();
+        } finally {
+            read.unlock();
+        }
+    }
+
+    public static void saveToCache(String id, Bitmap bitmap){
+        write.lock();
+        try {
+            cache.put(id, new SoftReference<Bitmap>(bitmap));
+        } finally {
+            write.unlock();
+        }
+    }
+
+    public static void clear() {
+        write.lock();
+        try {
+            cache.clear();
+        } finally {
+            write.unlock();
+        }
+    }
+
+}
