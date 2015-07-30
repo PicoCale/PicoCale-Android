4f55c7fc7baa6834ae322c186c4a7841ed785e3e
Jayakumaur <jaya.kumaur@gmail.com>
2015-07-30 02:56:09 -0400

    Modified notification based on image availability within radius.

--- app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/MainActivity.java
+++ app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/MainActivity.java
@@ -1,6 +1,8 @@
 package edu.cmu.mobileapp.picocale.view.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,16 +10,34 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 import edu.cmu.mobileapp.picocale.R;
+import edu.cmu.mobileapp.picocale.service.DeviceImageServiceImpl;
+import edu.cmu.mobileapp.picocale.service.ImageService;
+import edu.cmu.mobileapp.picocale.util.LocationUtils;
 
 
 public class MainActivity extends ActionBarActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Obtaining an instance of imageService to know
+        // whether there are images present within
+        // the current radius boundary
+        ImageService imageService = new DeviceImageServiceImpl();
+        int count = imageService.getLocationBasedImageList(this).size();
+        Log.i("---BOOL2CNT-->", Double.valueOf(count).toString());
+        Boolean isImageAvailable = false;
+        if(count>0)
+            isImageAvailable = true;
+        else
+            isImageAvailable = false;
+
+        //Calling the LocationService Class
         Intent serviceIntent = new Intent("edu.cmu.mobileapp.picocale.service.LocationService");
+        Log.i("---BOOL2AVL-->", isImageAvailable.toString());
+        serviceIntent.putExtra("isImageAvailable",isImageAvailable);
+        serviceIntent.putExtra("imageCount",count);
         startService(serviceIntent);
     }
 
