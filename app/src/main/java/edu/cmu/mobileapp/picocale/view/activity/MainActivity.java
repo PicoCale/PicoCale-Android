6933200866f0008ad2d7e7d6183cf3a3f57ee465
Srikrishnan Suresh <krishna.tanjavur@gmail.com>
2015-07-31 00:44:42 -0400

    Removing the unused menu from activity

--- app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/MainActivity.java
+++ app/src/main/java/edu/cmu/mobileapp/picocale/view/activity/MainActivity.java
@@ -42,27 +42,6 @@ public class MainActivity extends ActionBarActivity {
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_home, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     protected void onNewIntent(Intent intent) {
         //this is very important, otherwise you would get a null Scheme in the onResume later on.
         Log.i("Activity", "On resume");
